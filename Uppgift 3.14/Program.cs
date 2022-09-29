5a23916ce19a2a9c65633803762632868b6b0fe6
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-29 13:57:15 +0200

    Add project files.

---
+++ Uppgift 3.14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_3._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Låten du anger måste vara minst 2 min och 45 sek, och max 4 min 20 sek");
+            int tid = int.Parse(Console.ReadLine());
+            if (tid >= 245 && tid <= 420)
+            {
+                Console.WriteLine("Din låt skickas nu till P3 Radio");
+            }
+            else
+            {
+                Console.WriteLine("Din låt uppphör inte kraven för att spelas upp");
+            }
+        }
+    }
+}
